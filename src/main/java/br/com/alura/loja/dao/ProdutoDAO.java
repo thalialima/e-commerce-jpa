e07965039e7d4ca3c413c6c6d9970ea3840dae2e
Thalia Lima <thaliairene20@gmail.com>
2021-09-18 10:37:18 -0300

    Consultas com JPQL

--- src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -1,5 +1,8 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -16,5 +19,41 @@ public class ProdutoDAO {
 		this.em.persist(poduto);
 	}
 	
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id); //método que busca uma única entidade pelo id
+	}
 	
+	public List<Produto> buscarTodos(){
+		//utiliza-se a linguagem JPQL(SQL OO)
+		//a consulta é feita com o nome da entidade e utiliza-se um alias
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+	
+	public List<Produto> buscarPorNome(String nome){
+		//utiliza-se a linguagem JPQL(SQL OO)
+		//a consulta é feita com o nome da entidade e utiliza-se um alias
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public List<Produto> buscarPorNomeDaCategoria(String nome){
+		//utiliza-se a linguagem JPQL(SQL OO)
+		//a consulta é feita com o nome da entidade e utiliza-se um alias
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public BigDecimal buscarPrecoDoProdutoPeloNome(String nome){
+		//utiliza-se a linguagem JPQL(SQL OO)
+		//a consulta é feita com o nome da entidade e utiliza-se um alias
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, BigDecimal.class)
+				.setParameter("nome", nome)
+				.getSingleResult();
+	}
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,33 +1,62 @@
 package br.com.alura.loja.testes;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
+		cadastrarProduto();
+		
+		//consultas pelo id
+		Long id = 1l;
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		
+		Produto p = produtoDAO.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDAO.buscarPorNomeDaCategoria("CELULARES");
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+		
+		BigDecimal precoDoProduto = produtoDAO.buscarPrecoDoProdutoPeloNome("Xiaomi");
+		System.out.println("Preço do Produto: " + precoDoProduto);
+
+	
+	}
+
+	private static void cadastrarProduto() {
 		//instanciando uma entidade (transient)
 		Categoria celulares = new Categoria("CELULARES");
-
+		Produto celular = new Produto("Xiaomi", "Celular top", new BigDecimal("800"), celulares);
+		
+		
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
 		// inicia a transação
 		em.getTransaction().begin();
 
 		//(managed)
-		em.persist(celulares);
+		//em.persist(celulares);
 		
-		celulares.setNome("XPTO");
+		categoriaDAO.cadastrar(celulares);
+		produtoDAO.cadastrar(celular);
 		
-		em.flush();
-		em.clear();
 
 		// faz o commit no bd (BD)
 		em.getTransaction().commit();
 
 		// fecha o recurso  (detached)
 		em.close();
-	
 	}
 }
